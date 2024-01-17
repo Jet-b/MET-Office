330489f7043717f4adfa1f66d977c050a9d230fc
jonty <jontybeglin@gmail.com>
2024-01-17 16:18:42 +0000

    idk what to put here

--- Smoothed Normalised Probabilities.R
+++ Smoothed Normalised Probabilities.R
@@ -1,26 +1,31 @@
 # Set up a single plot
 par(mfrow = c(1, 1))
 
-# Remove missing values from the first dataset
+# data1 is the data without Human intervention
 data1 <- Mediterranean$Natural.forcings.only[!is.na(Mediterranean$Natural.forcings.only)]
 
-# Remove missing values from the second dataset
+# data2 is the data with Human intervention
 data2 <- Mediterranean$All.forcings.simulations[!is.na(Mediterranean$All.forcings.simulations)]
 
-# Plot the first smoothed normalized density line
-plot(density(data1), col = "lightgreen", main = "Overlayed Smoothed Normalized Densities", xlab = "Temperature difference", ylab = "Probability")
+plot(density(data1), col = "#129012", main = "Overlayed Smoothed Normalized Densities", xlab = "Temperature difference", ylab = "Probability")
 
-# Overlay the second smoothed normalized density line on the same axes
-lines(density(data2), col = "skyblue")
+lines(density(data2), col = "#2493c0")
 
-# Add a legend if needed
-legend("topright", legend = c("Natural", "Human"), fill = c("lightgreen", "skyblue"))
+legend("topright", legend = c("Natural", "Human"), fill = c("#129012", "#2493c0"))
 
-xValue <- 0
+xValue <- 3.71
 
 abline(v = xValue, col = "red", lty = 2)
 
-area1 <- integrate(function(data2) pnorm(data2, mean = mean(data2), sd = sd(data2)), xValue, max(data2))$value
+integrand_function1 <- function(x) dnorm(x, mean = mean(data1), sd = sd(data1))
+integrand_function2 <- function(x) dnorm(x, mean = mean(data2), sd = sd(data2))
 
+# Integrate from xValue to the maximum value of data2
+area1 <- integrate(integrand_function1, lower = xValue, upper = max(data1))$value
+area2 <- integrate(integrand_function2, lower = xValue, upper = max(data2))$value
 
-print(area1) 
+# Print the result
+
+print(area1)
+print(area2)
+print(area2/area1)
